5ff73f6bfddb691306695a2e4b285ebc2a96dcb1
Itz-ram <developer.cruzer@gmail.com>
2024-01-22 14:06:13 +0530

    Initial Files

---
+++ src/com/core/s1/javadoc/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.core.s1.javadoc;
+/**
+ * 
+ * Hello World application
+ * 
+ * @author ram
+ * @version HW-1.0.0
+ * 
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * This is our main entry of the program
+	 * 
+	 * {@code public static void main(String[] args)}
+	 * 
+	 * 
+	 * @param args arugments given during the execution of the program
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		//Single line comment 
+		/*
+		 * This is multi line comment
+		 */
+		System.out.println("Hi This is ramalinga");
+		//TODO this needs to be revisited
+	}
+
+}
+ 
---
+++ src/com/core/s1/primitives/PrimitiveTypes.java
@@ -0,0 +1,39 @@
+package com.core.s1.primitives;
+
+public class PrimitiveTypes {
+	public static void main(String[] args) {
+		int a = 1;
+		byte b= 12;
+		short s;
+		s =1;
+		
+		char c = 'a';
+		
+		long l = 1_000_000_000;
+		long y = 2_000_000_000_000L;//by default int will be used so if the range in outer than int we need to explicitly specify l or L for long
+		
+		double dd = 1.2;
+		float df = 1.2f;//by default double will be used. as double is larger than float we need to explicitly specify 1.2f or 1.2F; 
+		System.out.println(df);
+		
+		var ab = 12;
+//		ab = 123f;// we cannot reassign to some other data types in the var variables
+		ab = (int)123f; // so we can type cast it to convert the values to int 
+		
+		
+		char x = 100;// here default value is int but compiler can read literals so no need to type cast here
+		System.out.println(x);
+		
+		int a1 = 100;
+		char z = (char)a1;// here we need to type cast as it is already declared as int 
+		
+		
+		byte b1 = (byte)1000;
+		System.out.println(b1);
+		
+		long number = 499_999_999_000_000_001L;
+		double converted = (double)number;
+		System.out.println(number - (long)converted);
+	}
+
+}
