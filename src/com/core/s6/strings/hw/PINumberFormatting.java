cca43f428efc47924a4f972c3c7f46238f44a74e
Itz-ram <developer.cruzer@gmail.com>
2024-01-23 15:52:17 +0530

    Added String home work classes

---
+++ src/com/core/s6/strings/hw/PINumberFormatting.java
@@ -0,0 +1,13 @@
+package com.core.s6.strings.hw;
+
+public class PINumberFormatting {
+	
+	public static void main(String[] args) {
+		System.out.printf("%.1f,", Math.PI);
+		System.out.printf("%.2f,", Math.PI);
+		System.out.printf("%.3f,", Math.PI);
+		System.out.printf("%.4f,", Math.PI);
+		System.out.printf("%.5f", Math.PI);
+	}
+
+}
